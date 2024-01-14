cc2124f4225f0fb82064ec0ea989f5aff0aa2daa
Mustapha Rachidi <murachid@c4r9p9.1337.ma>
2024-01-14 13:10:11 +0100

    update

--- p2/confs/config.rb
+++ p2/confs/config.rb
@@ -1,8 +1,11 @@
 
-BOX_NAME = "bento/ubuntu-22.04"
+BOX_NAME =  "generic/debian12"
 
 MEMORY_SIZE = 1024
 CPU_COUNT = 1
 
 MASTER_HOSTNAME = "murachidS"
 MASTER_IP = "192.168.56.110"
+
+WORKER_HOSTNAME = "murachidSW"
+WORKER_IP = "192.168.56.111"
